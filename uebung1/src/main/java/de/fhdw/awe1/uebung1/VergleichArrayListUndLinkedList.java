26f7830949684dadadd47d88ee876f538f543500
Christian Heuermann <c.heuermann@gmx.de>
2018-03-25 16:41:30 +0200

    Erste Übung erstellt
    Vergleich von Array Implementierungen

---
+++ uebung1/src/main/java/de/fhdw/awe1/uebung1/VergleichArrayListUndLinkedList.java
@@ -0,0 +1,23 @@
+package de.fhdw.awe1.uebung1;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class VergleichArrayListUndLinkedList {
+
+	private List<String> arrayList = new ArrayList();
+	
+	private List<String> linkedList = new LinkedList(); 
+	
+	public static void main(String[] args) {
+		// 1. Vergleich: Mittleres Element lesen 
+
+		// 2. Vergleich: Element am Ende des Arrays schreiben
+		
+		// 3. Vergleich: Element in der Mitte des Arrays schreiben
+		
+		// 4. Vergleich: Element am Anfang des Arrays schreiben
+		
+	}
+}
