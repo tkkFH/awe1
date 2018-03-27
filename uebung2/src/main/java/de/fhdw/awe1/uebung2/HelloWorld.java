e3c375f87c1e24a1a3d007ed60d0f659dc0a0823
Christian Heuermann <c.heuermann@gmx.de>
2018-03-27 15:50:51 +0200

    Update HelloWorld.java

--- uebung2/src/main/java/de/fhdw/awe1/uebung2/HelloWorld.java
+++ uebung2/src/main/java/de/fhdw/awe1/uebung2/HelloWorld.java
@@ -7,6 +7,10 @@ public class HelloWorld {
 	public static void main(String[] args) {
 
 		System.out.println("Hallo Welt!");
+		System.out.println("Hallo Welt!");
+		System.out.println("Hallo Welt!");
+		System.out.println("Hallo Welt!");
+		System.out.println("Hallo Welt!");
 
 	}
 
