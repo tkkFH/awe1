7de7b7dfeec51bea95f559e6c59980317f0c01f5
Christian Heuermann <c.heuermann@gmx.de>
2018-03-27 16:15:24 +0200

    Neuer Branch

--- uebung2/src/test/java/de/fhdw/awe1/uebung2/HelloWorldTest.java
+++ uebung2/src/test/java/de/fhdw/awe1/uebung2/HelloWorldTest.java
@@ -4,6 +4,8 @@ import org.junit.Test;
 
 public class HelloWorldTest {
 
+	// Neuer Branch
+	
 	@Test
 	public void testMain() throws Exception {
 		throw new RuntimeException("not yet implemented");
