bbb59de6d462c3772bce20fad4c810b42dfeb870
Christian Heuermann <c.heuermann@gmx.de>
2018-03-27 16:01:57 +0200

    Revert "Lokaler Commit"
    This reverts commit 334fb213b4877af3e479bd7b0f3d38ae8a47b2b2.

--- uebung2/src/main/java/de/fhdw/awe1/uebung2/HelloWorld.java
+++ uebung2/src/main/java/de/fhdw/awe1/uebung2/HelloWorld.java
@@ -5,11 +5,13 @@ import java.time.LocalDateTime;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		if (args == null) {
-			System.out.println("Hallo Welt!");
-		} else {
-			System.out.println("Keine Parameter vorhanden...");
-		}
+
+		System.out.println("Hallo Welt!");
+		System.out.println("Hallo Welt!");
+		System.out.println("Hallo Welt!");
+		System.out.println("Hallo Welt!");
+		System.out.println("Hallo Welt!");
+
 	}
 
 }
